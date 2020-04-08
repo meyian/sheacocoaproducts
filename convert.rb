eb6c506692c42b2848f91c6853b33d65d7837c4d
Hassan Abudu <hassanabudu@Hassans-MacBook-Pro.local>
2020-04-08 08:40:24 +0000

    Tidy up root folder

--- convert.rb
+++ convert.rb
@@ -11,25 +11,3 @@ end
 
 # get list of filenames w/ extension html.haml
 # convert them to html
-
-=begin
-	
--- Aside --
-
--- Thu 19 Mar --
-
-- 7:41 AM -
-
-* Nerd.
-* Wow, the Strokes. I hope you listen to this and think of me. I got here first. Tagging it for us. One more bad decision.
-	
-- 7:53 AM -
-
-* Done
-
-
-
-
-
-=end
-
